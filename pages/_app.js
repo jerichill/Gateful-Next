df7f1622b03ee5e9305742af9dd0a9a27bf74881
J. Eric Hill <eric.hill@openmedium.biz>
2023-04-04 23:16:14 -0600

    /link

--- pages/_app.js
+++ pages/_app.js
@@ -28,7 +28,7 @@ export default function MyApp({ Component: Component, pageProps: pageProps }) {
   return (
     <>
  <Head>
-              <link href="https://fonts.googleapis.com/css2?family=Urbanist:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+              <link href="https://fonts.googleapis.com/css2?family=Urbanist:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet"></link>
               <script onload='LitJsSdk.litJsSdkLoadedInALIT()' src="https://jscdn.litgateway.com/index.web.js"></script>
 
                 </Head>
