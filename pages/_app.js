d16a24f93295ef1325fd3de74c5266560c51c2d6
J. Eric Hill <eric.hill@openmedium.biz>
2023-04-04 23:14:07 -0600

    Global Script

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,9 @@ import { useRouter } from "next/dist/client/router";
 import Head from "next/head";
 
 export default function MyApp({ Component: Component, pageProps: pageProps }) {
-  // Turbo Virtual protocol globle code implemantetion start
+   
+  // Gateful protocol global code implemantetion start
+  // if multiple checkAuth function is there than call in check function like, 
   const router = useRouter();
   const [displayPages, setDisplayPages] = useState(false);
   const check = async () => {
@@ -15,23 +17,21 @@ export default function MyApp({ Component: Component, pageProps: pageProps }) {
     }
   };
 
-  // function call to check Turbo Virtual gate script
+  // function call to check Gateful gate script
   React.useEffect(() => {
-    console.log(router, "router........");
     check();
   }, [router.asPath]);
 
-  // Turbo Virtual protocol globle code implemantetion end
+  // Gateful protocol global code implemantetion end
 
   React.useEffect(() => import("@lottiefiles/lottie-player"));
   return (
     <>
-      <Head>
-        <script
-          onload="LitJsSdk.litJsSdkLoadedInALIT()"
-          src="https://jscdn.litgateway.com/index.web.js"
-        ></script>
-      </Head>
+ <Head>
+              <link href="https://fonts.googleapis.com/css2?family=Urbanist:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+              <script onload='LitJsSdk.litJsSdkLoadedInALIT()' src="https://jscdn.litgateway.com/index.web.js"></script>
+
+                </Head>
       {displayPages && <Component {...pageProps} />}
     </>
   );
