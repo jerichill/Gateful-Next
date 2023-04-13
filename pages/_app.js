95b1cf7616fd0d662d9250951d4d7092b96b6c90
J. Eric Hill <eric.hill@openmedium.biz>
2023-04-13 08:50:51 -0600

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,25 @@
 import "./style.css";
+import checkAuth from "../public/js/gateful.js file (src/js/gateful.js";
+import { useRouter } from "next/dist/client/router";
+import Head from "next/head";
+
+// Gateful protocol global code implemantetion start
+// if multiple checkAuth function is there than call in check function like, 
+  const router = useRouter();
+  const [displayPages, setDisplayPages] = useState(false);
+  const check = async () => {
+    const result = await checkAuth();
+    if (result) {
+      setDisplayPages(true);
+    }
+  };
+
+  // function call to check Gateful gate script
+  React.useEffect(() => {
+    check();
+  }, [router.asPath]);
+
+  // Gateful protocol global code implemantetion end
 
 import React from "react";
 export default function MyApp({
@@ -7,4 +28,15 @@ export default function MyApp({
 }) {
   React.useEffect(() => import("@lottiefiles/lottie-player"));
   return <Component {...pageProps} />;
-}
+
+ return (
+    <>
+      <Head>
+        <script
+          onload="LitJsSdk.litJsSdkLoadedInALIT()"
+          src="https://jscdn.litgateway.com/index.web.js"
+        ></script>
+      </Head>
+      {displayPages && <Component {...pageProps} />}
+    </>
+  );
