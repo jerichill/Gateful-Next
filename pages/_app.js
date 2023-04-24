db4162feee1a98f47ec24eccb34b96031172bd7f
tejaskateshiya <tejas.kateshiya@teksun.in>
2023-04-24 14:29:06 +0530

    update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,6 @@ import "./style.css";
 import React, { useState, useRef, useEffect } from "react";
 import checkAuth from "../public/js/gateful.js";
 import checkAuth2 from "../public/js/gateful2.js";
-import Script from 'next/script'
 
 import { useRouter } from "next/dist/client/router";
 import Head from "next/head";
@@ -39,7 +38,7 @@ export default function MyApp({ Component: Component, pageProps: pageProps }) {
         <link href="https://fonts.googleapis.com/css2?family=Urbanist:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet"></link>
         <script onload='LitJsSdk.litJsSdkLoadedInALIT()' src="https://jscdn.litgateway.com/index.web.js"></script>
 
-        <Script
+        <script
           src="https://d3fvarydh99mx0.cloudfront.net/gateful3.js"
           onLoad={() => {
             const check = checkAuth3();
