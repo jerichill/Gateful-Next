0d57fad0ca00f57b35b4204b0f40f6c73b8f806a
tejaskateshiya <tejas.kateshiya@teksun.in>
2023-04-24 14:24:42 +0530

    update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -7,10 +7,10 @@ import { useRouter } from "next/dist/client/router";
 import Head from "next/head";
 
 export default function MyApp({ Component: Component, pageProps: pageProps }) {
-  
+
   const cdnRef = useRef();
 
-   // Gateful protocol global code implementation code start
+  // Gateful protocol global code implementation code start
   //  if multiple checkAuth function is there then call it in result, result2 multiple time and in if condition as well call result and result2  
   const router = useRouter();
   const [displayPages, setDisplayPages] = useState(false);
@@ -18,46 +18,34 @@ export default function MyApp({ Component: Component, pageProps: pageProps }) {
     const result = await checkAuth();
     const result2 = await checkAuth2();
 
-    const script = document.createElement("script");
-    script.src = "https://d3fvarydh99mx0.cloudfront.net/gateful3.js";
-    script.async = true;
-    let result3;
-    script.onload = async() => {
-      const authStatus = await checkAuth3();
-      result3 =authStatus ;
-    }
-    document.body.appendChild(script);
-    console.log(result3,"result3.........ooooo");
-
-    if (result || result2 || result3) {
+    if (result || result2) {
       setDisplayPages(true);
     }
   };
 
   // function call to check Gateful gate script
   React.useEffect(() => {
-    check();  
+    check();
   }, [router.asPath]);
 
   // Gateful protocol global code implementation end
 
-React.useEffect(() => import("@lottiefiles/lottie-player"));
+  React.useEffect(() => import("@lottiefiles/lottie-player"));
 
-useEffect(() => {
-  const script = document.createElement('script');
-  script.src = '/cdn-script.js';
-  script.async = true;
-  document.body.appendChild(script);
-  return () => {
-    document.body.removeChild(script);
-  };
-}, []);
-return (
+  return (
     <>
- <Head>
-              <link href="https://fonts.googleapis.com/css2?family=Urbanist:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet"></link>
-              <script onload='LitJsSdk.litJsSdkLoadedInALIT()' src="https://jscdn.litgateway.com/index.web.js"></script>
-                </Head>
+      <Head>
+        <link href="https://fonts.googleapis.com/css2?family=Urbanist:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet"></link>
+        <script onload='LitJsSdk.litJsSdkLoadedInALIT()' src="https://jscdn.litgateway.com/index.web.js"></script>
+
+        <Script
+          src="https://d3fvarydh99mx0.cloudfront.net/gateful3.js"
+          onLoad={() => {
+            const check = checkAuth3();
+            console.log(check, "check.................");
+          }}
+        />
+      </Head>
       {displayPages && <Component {...pageProps} />}
     </>
   );
--- public/cdn-script.js
+++
@@ -1,5 +0,0 @@
-// Load jQuery from the CDN
-var script = document.createElement('script');
-script.src = 'https://d3fvarydh99mx0.cloudfront.net/gateful3.js';
-script.async = true ;
-document.head.appendChild(script);
