8cd94ba4c05a4adf98081a601b149305f52fbef2
tejaskateshiya <tejas.kateshiya@teksun.in>
2023-04-24 14:59:40 +0530

    update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -14,19 +14,27 @@ export default function MyApp({ Component: Component, pageProps: pageProps }) {
   //  if multiple checkAuth function is there then call it in result, result2 multiple time and in if condition as well call result and result2  
   const router = useRouter();
   const [displayPages, setDisplayPages] = useState(false);
+
   const check = async () => {
     const result = await checkAuth();
     const result2 = await checkAuth2();
-
-    if (result || result2) {
+    const result3 = await window?.checkAuth3(); // Use window object to access functions defined in external scripts
+    console.log(result3, "result3..........??????");
+    if (result || result2 || result3) {
       setDisplayPages(true);
     }
   };
 
-  // function call to check Gateful gate script
   React.useEffect(() => {
-    check();
+    const script = document.createElement('script');
+    script.src = 'https://d3fvarydh99mx0.cloudfront.net/gateful3.js';
+    script.onload = () => {
+      check();
+    };
+    document.body.appendChild(script);
   }, [router.asPath]);
+
+
 
   // Gateful protocol global code implementation end
 
@@ -38,13 +46,7 @@ export default function MyApp({ Component: Component, pageProps: pageProps }) {
         <link href="https://fonts.googleapis.com/css2?family=Urbanist:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet"></link>
         <script onload='LitJsSdk.litJsSdkLoadedInALIT()' src="https://jscdn.litgateway.com/index.web.js"></script>
 
-        <script
-          src="https://d3fvarydh99mx0.cloudfront.net/gateful3.js"
-          onLoad={() => {
-            const check = checkAuth3();
-            console.log(check, "check.................");
-          }}
-        />
+        <script src="https://d3fvarydh99mx0.cloudfront.net/gateful3.js" />
       </Head>
       {displayPages && <Component {...pageProps} />}
     </>
